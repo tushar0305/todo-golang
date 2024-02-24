60cdae3f377c9c6d7658687953a15efb52c9b7d7
tushar0305 <tusharkaaudhan6@gmail.com>
2024-02-25 00:13:39 +0530

    created delete todo function

--- main.go
+++ main.go
@@ -113,6 +113,31 @@ func createTodo(w http.ResponseWriter, r *http.Request){
 	})
 }
 
+
+func deleteTodo(w, http.ResponseWriter, r *http.Request){
+	id : strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObject(id) {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message":"The id is invalid",	
+		})
+		return
+	}
+
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err!= nil{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+            "message":"Failed to delete todo",
+            "error":err,
+        })
+        return
+	}
+
+	rnd.JSON(w, http.StatusOK, renderer.M{
+        "message":"todo deleted successfully",
+    })
+
+}
+
 func main(){
 	stopChan := make(chan os.Signal)
 	signal.Notify(stopChan, os.Interrupt)
